cd40df2af86c4a8607ceb1e7a14d7c89d2681b7a
Shivam Vashisth <vashisth11shivam@gmail.com>
2024-11-23 16:14:37 +0530

    initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+function layout({ children } : {children : React.ReactNode
+}) {
+  return (
+    <div className="bg-red-500 h-full">
+        {children}
+    </div>
+  )
+}
+
+export default layout
