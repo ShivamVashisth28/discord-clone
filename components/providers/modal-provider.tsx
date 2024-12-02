5e0ae12bb5a6b98a11bb45a969a2a0b24ceaace4
Shivam Vashisth <vashisth11shivam@gmail.com>
2024-12-02 23:44:55 +0530

    manage members

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,9 +3,9 @@
 import React, { useEffect, useState } from "react";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
-// import { InviteModal } from "@/components/modals/invite-modal";
-// import { EditServerModal } from "@/components/modals/edit-server-modal";
-// import { MembersModal } from "@/components/modals/members-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 // import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 // import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 // import { DeleteServerModal } from "@/components/modals/delete-server-modal";
@@ -26,10 +26,10 @@ export function ModalProvider() {
   return (
     <>
       <CreateServerModal />
-      {/* <InviteModal />
+      <InviteModal />
       <EditServerModal />
       <MembersModal />
-      <CreateChannelModal />
+      {/* <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
